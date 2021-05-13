127ba3386539051b55a4c7562f43baee18005a5c
Alvaro Gomez <alvarobst1@gmail.com>
2021-05-13 19:30:07 +0200

    Prueba MySqlConnection

--- BualaTok/routes/index.js
+++ BualaTok/routes/index.js
@@ -1,5 +1,26 @@
 var express = require('express');
-var router = express.Router();
+const mysql = require('mysql'); // <- Conexión con la BBDD
+const app = require('../app');
+var router = express.Router();   
+
+
+// ### MySql INIT
+const conexion = mysql.createConnection(
+  {
+  host:'localhost:3306',
+  user:'root',
+  password:'',
+  database:'daweb'
+  });
+
+  //### Check Connect BBDD
+  conexion.connect(function (error){
+    if (error)
+    console.log('Problemas de conexion con mysql');
+    });
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,7 +35,13 @@ router.get('/registrarProducto', function(req, res, next) {
   res.render('registrarProducto', {});
 });
 
+
+
 router.get('/login', function(req, res, next) {
+  const sql = " INSERT INTO daweb.usuario (nombre, apellidos, usuario, clave, credito, provincia, mail) VALUES ('Jose', 'Gutierrez Fernandez', 'josegu', 'jose123', 45, 'Murcia', 'josegu@um.es');";
+  conexion.query(sql, (error, results) => {
+    if (error) throw error;
+  });
   res.render('sigIn', {});
 });
 router.get('/bucador', function(req, res, next) {
