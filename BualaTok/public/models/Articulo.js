ab7d22472e41f1b0d3639cd26f4edc9e11828f28
JorgeCabezos <jorge.cabezos.casado@gmail.com>
2021-05-21 13:31:19 +0200

    registro de articulos implementado

--- BualaTok/public/models/Articulo.js
+++ BualaTok/public/models/Articulo.js
@@ -1,5 +1,5 @@
 class Articulo {
-    constructor(nombre, descripcion, precio, categoria, estado, foto, fecha) {
+    constructor(nombre, precio, descripcion, foto, fecha, categoria, estado, idUsuario) {
     this.nombre = nombre;
     this.precio = precio;
     this.descripcion = descripcion;
@@ -7,13 +7,14 @@ class Articulo {
     this.fecha = fecha;
     this.categoria = categoria;
     this.estado = estado;
+    this.idUsuario = idUsuario;
     this.visualizaciones = 0;
     }
 }
 
-createArticulo = (nombre, descripcion, precio, categoria, estado, foto, fecha) => {
+createArticulo = (nombre, precio, descripcion, foto, fecha, categoria, estado, idUsuario) => {
     try {
-        return new Articulo(nombre, descripcion, precio, categoria, estado, foto, fecha);
+        return new Articulo(nombre, precio, descripcion, foto, fecha, categoria, estado, idUsuario);
     } catch (error) {
         console.log(error);
     }
--- BualaTok/routes/registrarArticuloController.js
+++ BualaTok/routes/registrarArticuloController.js
@@ -16,52 +16,26 @@ router.post("/registrarArticulo", function (req, res, next) {
     var nombre = req.body.name;
     var descripcion = req.body.descripcion;
     var precio = req.body.precio;
-    var categoria = Categoria.obtenerCategoria(req.body.categoria);
-    var estado = Estado.obtenerEstado(req.body.radiobuttonset);
+    var categoria = req.body.categoria;
+    var estado = req.body.estado;
 
-   
+    var idUsuario = '1';
 
-    //var foto = 'foto.jpg';
+    var foto = 'foto.jpg';
     var f = new Date();
-    const fecha = f.getDate() + "-"+ (f.getMonth()+1)+ "-" +f.getFullYear();
-    var foto = req.body.foto;
+    const fecha = f.getFullYear() + "-"+ (f.getMonth()+1)+ "-" +f.getDate();
     console.log("Hago el post");
 
-    var articulo = Articulo.createArticulo(nombre, descripcion, precio, categoria, estado, foto, fecha);
+    var articulo = Articulo.createArticulo(nombre, precio, descripcion, foto, fecha, categoria, estado, idUsuario);
     console.log(articulo);
     var sql = ('INSERT INTO daweb.articulo SET ?');
     var paramet = [articulo];
     const rows = conexion.query(
         sql, paramet, (error, results) => {
+            console.log(error);
         }
     );
     res.render('index', { title: nombre, contrasena: precio });
 });
-/*
-function readFile(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            var filePreview = document.createElement('img');
-            filePreview.id = 'file-preview';
-            //e.target.result contents the base64 data from the image uploaded
-            filePreview.src = e.target.result;
-            console.log(e.target.result);
-
-            var previewZone = document.getElementById('file-preview-zone');
-            previewZone.appendChild(filePreview);
-        }
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-var foto = document.getElementById('');
-fileUpload.onchange = function (e) {
-readFile(e.srcElement);
-}
-*/
-
 
 module.exports = router;
