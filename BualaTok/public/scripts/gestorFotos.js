454b0a9d37c924169fd08b70c79c174c62feef6e
Alvaro Gomez <alvarobst1@gmail.com>
2021-06-03 19:37:53 +0200

    mostrarFoto en Index

--- BualaTok/public/scripts/gestorFotos.js
+++ BualaTok/public/scripts/gestorFotos.js
@@ -1,42 +1,7 @@
 
 window.guardarImagen = guardarImagen;
-window.mostrar = mostrar;
-/*window.onload = function() {
+window.onload = function() {
 
-    console.log("Se usa el onload")
-    var fileDisplayArea = document.getElementById('fileDisplayArea');
-    var fileInput = document.getElementById("myImage");
-
-    fileInput.addEventListener('change', function(e) {
-        var file = fileInput.files[0];
-        var imageType = /image.;
-
-        if (file.type.match(imageType)) {
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-                fileDisplayArea.innerHTML = "";
-
-                var img = new Image();
-                img.src = reader.result;
-
-
-                //var myFile = new File([img], "NoMeMareesLaPerdiz.png", {type: "text/image; charset=ISO-8859-1"});
-                //saveAs(myFile)
-            
-
-                fileDisplayArea.appendChild(img);
-            }
-
-            reader.readAsDataURL(file);	
-        } else {
-            fileDisplayArea.innerHTML = "File not supported!"
-        }
-    });
-
-}
-*/
-function mostrar(){
     console.log("Se usa el onload")
     var fileDisplayArea = document.getElementById('fileDisplayArea');
     var fileInput = document.getElementById("myImage");
@@ -67,9 +32,10 @@ function mostrar(){
             fileDisplayArea.innerHTML = "File not supported!"
         }
     });
+
 }
 
 function guardarImagen(){
     $('#myImage').trigger('click');
     
-    }
+}
